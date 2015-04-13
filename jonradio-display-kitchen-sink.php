48ad92f17b551fd4ce88e11b7b58b098d77f97cc
adiant <adiant@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-13 15:30:55 +0000

    Adoption
    git-svn-id: https://plugins.svn.wordpress.org/jonradio-display-kitchen-sink/tags/3.1@1133792 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jonradio-display-kitchen-sink.php
+++ jonradio-display-kitchen-sink.php
@@ -4,7 +4,7 @@ Plugin Name: jonradio Display Kitchen Sink
 Plugin URI: http://jonradio.com/plugins/jonradio-display-kitchen-sink/
 Description: All users will have the Kitchen Sink displayed in Visual mode for both the Page and Post Editors.
 Version: 3.1
-Author: jonradio
+Author: dgewirtz, jonradio
 Author URI: http://jonradio.com/plugins
 License: GPLv2
 */
